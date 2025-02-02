899501ca442dcd779ab6676b3902ea0d05de0133
Strictviral <jochie2478@gmail.com>
2025-02-02 14:38:39 -0500

    GameplayEffects funtionnalityyy added to potions, frennemies actor refactored

--- Source/Frenemies/Private/FrenemiesActors/FrenemiesEffectActor.cpp
+++ Source/Frenemies/Private/FrenemiesActors/FrenemiesEffectActor.cpp
@@ -4,9 +4,8 @@
 #include "FrenemiesActors/FrenemiesEffectActor.h"
 
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/FrenemiesAttributeSet.h"
-#include "Components/SphereComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+
 
 
 AFrenemiesEffectActor::AFrenemiesEffectActor()
@@ -14,42 +13,43 @@ AFrenemiesEffectActor::AFrenemiesEffectActor()
  	
 	PrimaryActorTick.bCanEverTick = false;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
+
 }
 
 void AFrenemiesEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	//call back function fire off in response to something overlapping with our sphere component
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AFrenemiesEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &AFrenemiesEffectActor::EndOverlap);
-}
-
-
-void AFrenemiesEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	//TODO: Change this to apply a gameplay effect, for now we are const_cast as a hack!
-	if( IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		///Return the attribute set of the other actor that overlaps with us
-		const UFrenemiesAttributeSet* FrenemiesAttributeSet = Cast<UFrenemiesAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UFrenemiesAttributeSet::StaticClass()));
-		UFrenemiesAttributeSet* MutableFrenemiesAttributeSet = const_cast<UFrenemiesAttributeSet*>(FrenemiesAttributeSet);
-		MutableFrenemiesAttributeSet->SetHealth(FrenemiesAttributeSet->GetHealth() + 25.f);
-		MutableFrenemiesAttributeSet->SetStamina(FrenemiesAttributeSet->GetStamina() - 25.0f);
-		Destroy();
-	}
-}
-
-void AFrenemiesEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
+	
 	
 }
+
+void AFrenemiesEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	//get ability system component using unreal engines default function in the Ability system Library
+	UAbilitySystemComponent* TargetAbilitySystemComponent =  UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	if (TargetAbilitySystemComponent == nullptr)
+	{
+		return;
+	}
+
+	check(GameplayEffectClass);
+	
+	FGameplayEffectContextHandle EffectContextHandle =  TargetAbilitySystemComponent->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	
+	FGameplayEffectSpecHandle EffectSpecHandle = TargetAbilitySystemComponent->MakeOutgoingSpec(GameplayEffectClass,1.f, EffectContextHandle);
+	
+	TargetAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	
+}
+
+
+
+
+
+
 
 
 
--- Source/Frenemies/Public/FrenemiesActors/FrenemiesEffectActor.h
+++ Source/Frenemies/Public/FrenemiesActors/FrenemiesEffectActor.h
@@ -6,7 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "FrenemiesEffectActor.generated.h"
 
-class USphereComponent;
+class UGameplayEffect;
+
 
 UCLASS()
 class FRENEMIES_API AFrenemiesEffectActor : public AActor
@@ -16,22 +17,19 @@ class FRENEMIES_API AFrenemiesEffectActor : public AActor
 public:	
 	AFrenemiesEffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	
 protected:
 
 	virtual void BeginPlay() override;
 
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+
 private:
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
-
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
+	
 
 };
